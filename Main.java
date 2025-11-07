5ed66beecd155282ad1763acc0a04c42ffab6ced
nelsinho-32 <110434694+nelsinho-32@users.noreply.github.com>
2025-11-07 14:45:15 -0300

    Refactor student addition and retrieval logic

--- Main.java
+++ Main.java
@@ -2,18 +2,18 @@ public class Main {
     public static void main(String[] args) {
         Turma t1 = new Turma();
         Aluno aluno1 = new Aluno("Nelson", "100", 0, 0);
-        t1.adicionarAluno(aluno1);
-        
         Aluno aluno2 = new Aluno("Nelson Filho", "101", 0, 0);
+        t1.adicionarAluno(aluno1);
         t1.adicionarAluno(aluno2);
+        t1.adicionarAluno(new Aluno("Carol", "102", 10, 10));
 
-        Aluno alunoEn = t1.buscarPorMatricula("100");
+        t1.listarAlunos();
 
-        if(alunoEn != null){
-            alunoEn.exibirInfo();
+        Aluno buscado = t1.buscarPorMatricula("102");
+
+        if(buscado != null){
+            buscado.exibirInfo();
         }
-
-        // t1.listarAlunos();
 
     }
 
